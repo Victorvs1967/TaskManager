9fbeda3db3c7211cfad1f4e303e8ba6ca3e6ac88
Victor <victorsmirnov@me.com>
2022-01-09 15:52:14 +0200

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Victor Smirnov on 09.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
